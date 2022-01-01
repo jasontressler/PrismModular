2183c1ca01502c3866bd99ba7696148af8e618ad
Jason Tressler <jasontressler@gmail.com>
2022-01-01 15:40:33 -0500

    Wrap navigation commands

---
+++ Infrastructure/Common/INavigator.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Commands;
+
+namespace Infrastructure.Common
+{
+    public interface INavigator
+    {
+        public DelegateCommand NavigateConfigureCommand { get; }
+        public DelegateCommand NavigateProcessCommand { get; }
+        public DelegateCommand ExitCommand { get; }
+
+    }
+}
