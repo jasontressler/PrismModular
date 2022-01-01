c4a936eb973854032420b040a7cfa67a8d931133
Jason Tressler <jasontressler@gmail.com>
2022-01-01 13:26:03 -0500

    Update shared resources project

--- Infrastructure/Constants/ModuleNames.cs
+++ Infrastructure/Constants/ModuleNames.cs
@@ -11,6 +11,8 @@ namespace Infrastructure.Constants
         public const string MENU_REGION = "MenuRegion";
         public const string NAV_REGION = "NavRegion";
         public const string STATUS_REGION = "StatusRegion";
-        public const string MAIN_REGION = "MainRegion";
+        public const string MAIN_SPLASH = "Splash";
+        public const string MAIN_CONFIGURE = "Configure";
+        public const string MAIN_PROCESS = "Process";
     }
 }
