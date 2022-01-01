32bbb63305dccc8045e4179e23375262fc4cb7d9
Jason Tressler <jasontressler@gmail.com>
2022-01-01 15:40:55 -0500

    Add Exit method

--- Infrastructure/Common/NavigationService.cs
+++ Infrastructure/Common/NavigationService.cs
@@ -12,6 +12,7 @@ namespace Infrastructure.Common
     {
         public void NavigateToProcess();
         public void NavigateToConfigure();
+        public void Exit();
     }
 
     public class NavigationService : INavigationService
@@ -26,5 +27,7 @@ namespace Infrastructure.Common
         public void NavigateToConfigure() => _regionManager.RequestNavigate(RegionNames.SHELL_MAIN_REGION, ModuleNames.MAIN_CONFIGURE);
         
         public void NavigateToProcess() => _regionManager.RequestNavigate(RegionNames.SHELL_MAIN_REGION, ModuleNames.MAIN_PROCESS);
+
+        public void Exit() => Environment.Exit(0);
     }
 }
